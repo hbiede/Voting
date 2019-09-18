fba4693ef5bc1571628765cfc5ef1e565a9a3ab2
Hundter Biede <hbiede@gmail.com>
2019-09-18 18:49:32 -0500

    Starting Admin Dashboard - HBiede

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,7 +1,20 @@
 <?php $_REQUEST['title'] = 'Admin Dashboard';
-include("php/header.php");?>
+include("php/header.php");
+confirmLoggedInAdmin();
 
-<?php include("php/navbar.php");?>
+include("php/navbar.php");?>
 <!-- TODO: EVERYTHING -->
+
+    <h1>Admin Dashboard</h1>
+
+    <div class="card">
+        <h5 class="card-header">Elections</h5>
+
+        <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="election_editor.php" class="btn btn-primary">Create Election</a>
+        </div>
+    </div>
 
 <?php include("php/footer.php");?>
