a85940c7ed9ffc38e7e93c126d808237f4b62b70
Hundter Biede <hbiede@gmail.com>
2019-09-17 22:04:01 -0500

    Login page v1 ✅ - HBiede

--- login.php
+++ login.php
@@ -1,9 +1,101 @@
 <?php
 $_REQUEST['title'] = 'Login';
-include("php/header.php");?>
+include("php/header.php");
+include("php/navbar.php");
 
-<?php include("php/navbar.php");?>
-<!-- TODO: EVERYTHING -->
+if (loggedInSuperAdmin()) {
+    redirect("superadmin_dashboard.php");
+} elseif (loggedInAdmin()) {
+    redirect("admin_dashboard.php");
+} elseif (loggedInVoter()) {
+    redirect("voting.php");
+}
+
+global $connection;
+if (isset($_GET['username']) || isset($_POST['username'])) {
+    if ((isset($_GET['username']) && isset($_GET['password'])) || (isset($_POST['username']) && isset($_POST['password']))) {
+        if ($_POST['si'] == 'Sign in - Admin') {
+            $username = prepMySQLString(isset($_POST['username']) ? $_POST['username'] : $_GET['username']);
+            $password = sha1(prepMySQLString(isset($_POST['password']) ? $_POST['password'] : $_GET['password']));
+
+            // entered a password
+            if (!empty($username) && !empty($password)) {
+                $query = "SELECT username,password,isSuperAdmin FROM admins WHERE username='{$username}'";
+
+                // SQL error prevention
+                if ($executeQuery = mysqli_query($connection, $query)) {
+
+                    // only one such voterID
+                    if ((mysqli_num_rows($executeQuery) == 1)) {
+                        while ($row = mysqli_fetch_assoc($executeQuery)) {
+                            $dbPassword = $row['password'];
+
+                            // password match detection
+                            if ($password == $dbPassword) {
+                                $_SESSION['username'] = $row['username'];
+                                if ($row['isSuperAdmin'] == 0) {
+                                    $_SESSION['admin'] = $username;
+                                    redirect("admin_dashboard.php");
+                                } else {
+                                    $_SESSION['superAdmin'] = $username;
+                                    redirect("superadmin_dashboard.php");
+                                }
+                            } else {
+                                $_SESSION['error'] = "Enter correct username and password";
+                                redirect("login.php");
+                            }
+                        }
+                    } else {
+                        $_SESSION['error'] = "Enter correct username and password";
+                        redirect("login.php");
+                    }
+                }
+            } else {
+                $_SESSION['error'] = "Fill all fields";
+                redirect("login.php");
+            }
+        } else {
+            $voterID = prepMySQLString(isset($_POST['username']) ? $_POST['username'] : $_GET['username']);
+            $password = sha1(prepMySQLString(isset($_POST['password']) ? $_POST['password'] : $_GET['password']));
+
+            // entered a password
+            if (!empty($voterID) && !empty($password)) {
+                $query = "SELECT voterID,password FROM users WHERE voterID='{$voterID}'";
+
+                // SQL error prevention
+                if ($executeQuery = mysqli_query($connection, $query)) {
+
+                    // only one such voterID
+                    if ((mysqli_num_rows($executeQuery) == 1)) {
+                        while ($row = mysqli_fetch_assoc($executeQuery)) {
+                            $dbPassword = $row['password'];
+
+                            // password match detection
+                            if ($password == $dbPassword) {
+                                $_SESSION['voterID'] = $row['voterID'];
+                                redirect("voting.php");
+                            } else {
+                                $_SESSION['error'] = "Enter correct username and password";
+                                redirect("login.php");
+                            }
+                        }
+                    } else {
+                        $_SESSION['error'] = "Enter correct username and password";
+                        redirect("login.php");
+                    }
+                }
+            } else {
+                $_SESSION['error'] = "Fill all fields";
+                redirect("login.php");
+            }
+        }
+    } else {
+        $_SESSION['error'] = "Fill all fields";
+        redirect("login.php");
+    }
+}
+
+?>
 
     <form class="form-signin" style="padding-top: 50px;" action="login.php" method="POST">
         <div class="text-center mb-4">
@@ -12,15 +104,17 @@ include("php/header.php");?>
         </div>
 
         <div class="form-label-group">
-            <input type="text" id="voterID" name="voterID" class="form-control" placeholder="Voter ID" required="" autofocus="">
-            <label for="voterID">Voter ID</label>
+            <input type="text" id="username" name="username" class="form-control" placeholder="Username" required="" autofocus="">
+            <label for="username">Username</label>
         </div>
 
         <div class="form-label-group">
-            <input type="password" id="inputPassword" name="inputPassword" class="form-control" placeholder="Password" required="">
-            <label for="inputPassword">Password</label>
+            <input type="password" id="password" name="password" class="form-control" placeholder="Password" required="">
+            <label for="password">Password</label>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <p class="text-center" style="color:darkred"><?php echo $_SESSION['error']; unset($_SESSION['error'])?></p>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="si" value="Sign in">Sign in</button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="si" value="Sign in - Admin">Sign in - Admin</button>
         <p class="mt-5 mb-3 text-muted text-center">©2019 - Hundter Biede</p>
     </form>
 
