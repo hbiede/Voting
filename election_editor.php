32bbed367acd48d92c9b07b3443e048f6410b36f
Hundter Biede <hbiede@gmail.com>
2019-09-18 18:49:52 -0500

    Rough draft of a drag and drop orderable list - HBiede

---
+++ election_editor.php
@@ -0,0 +1,46 @@
+<?php
+$_REQUEST['title'] = 'Election Editor';
+include("php/header.php");
+
+include("php/navbar.php");?>
+
+<h1>Election Editor</h1>
+
+<ul class="election-editor-list">
+    <li>
+        <div class="alert alert-secondary text-center" role="form">1</div>
+    </li>
+    <li>
+        <div class="alert alert-secondary text-center" role="form">2</div>
+    </li>
+    <li id="add">
+        <div class="alert alert-secondary text-center" role="form" onclick="addItemAbove(this);">+</div>
+    </li>
+</ul>
+
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
+<script>
+    $( function() {
+            $( ".election-editor-list" ).sortable({
+                connectWith: '.connected',
+                start: function (event, ui) {
+                    if (ui.item[0].id === "add") {
+                        addItemAbove(ui.item)
+                    }
+                },
+                update: function (event, ui) {
+                    //
+                }
+            })
+            $( ".election-editor-list" ).disableSelection();
+        }
+    );
+
+    function addItemAbove(uiItem) {
+        window.alert("add");
+    }
+</script>
+
+<?php include("php/footer.php");?>
+
