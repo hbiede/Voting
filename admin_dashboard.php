84d5f2f8f0b082b4e2132328e1f387d8090b260f
Hundter Biede <hbiede@gmail.com>
2019-09-20 22:06:57 -0500

    Format cards for dashboard - HBiede

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -5,14 +5,29 @@ confirmLoggedInAdmin();
 <!-- TODO: EVERYTHING -->
 
     <h1>Admin Dashboard</h1>
+<?php if (isset($_SESSION['error'])) {
+    echo '<div class="alert alert-danger text-center php-error-alert" role="alert">'.$_SESSION['error'].'</div>';
+} unset($_SESSION['error']);?>
 
-    <div class="card">
-        <h5 class="card-header">Elections</h5>
+    <div class="container">
+        <div class="row"> <!-- 3 items per row -->
+            <div class="card">
+                <h5 class="card-header" style="width: 18rem;">Election Creator</h5>
 
-        <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="election_editor.php" class="btn btn-primary">Create Election</a>
+                <div class="card-body">
+                    <h5 class="card-title">Create a new election</h5>
+                    <a href="election_editor.php" class="btn btn-primary">Create Election</a>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <h5 class="card-header">Election Creator</h5>
+
+                <div class="card-body">
+                    <h5 class="card-title">Create a new election</h5>
+                    <a href="election_editor.php" class="btn btn-primary">Create Election</a>
+                </div>
+            </div>
         </div>
     </div>
 
