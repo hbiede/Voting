8b856692d98ee37bfe3bf582a68e95603877360e
Hundter Biede <hbiede@gmail.com>
2019-09-24 01:17:54 -0500

    Add drop down list for organizations. Button tag is incomplete - HBiede

--- organization_picker.php
+++ organization_picker.php
@@ -7,6 +7,34 @@ confirmLoggedInSuperAdmin();
 
 <!-- TODO: EVERYTHING -->
 
+<form action="organization_picker.php" method="POST">
+
+    <?php
+        global $connection;
+        $query = "SELECT id,organizationName FROM organizations;";
+
+        // SQL error prevention
+        if ($executeQuery = mysqli_query($connection, $query)) {
+            if ((mysqli_num_rows($executeQuery) > 0)) {
+                echo '<select name="orgIDSelection">';
+                while ($row = mysqli_fetch_assoc($executeQuery)) {
+                    $orgID = $row['id'];
+                    $orgName = $row['organizationName'];
+                    echo "<option value=\"$orgID\">$orgName</option>";
+                }
+                echo '</select>';
+                echo '<input type="submit" class="btn btn-primary">';
+            } else {
+                echo '<div class="alert alert-danger text-center php-error-alert" role="alert">No Organizations Exist</div>';
+            }
+        } else {
+            echo '<div class="alert alert-danger text-center php-error-alert" role="alert">SQL Error</div>';
+        }
+
+    ?>
+
+</form>
+
 
 
 <?php include("php/footer.php");?>
